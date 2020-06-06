832f4c98a4b91948791c5794d9c75c67565e1381
thalesleite <thalesaleite@gmail.com>
2020-06-06 11:03:50 +0100

    fetch states and city from api

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import axios from 'axios';
 import api from '../../services/api';
 
 import './styles.css';
@@ -15,14 +16,54 @@ interface Item {
   image_url: string
 }
 
+interface State {
+  sigla: string
+}
+
+interface City {
+  nome: string
+}
+
 const CreatePoint = () => {
-  const [ items, setItems ] = useState<Array<Item>>([]);
+  const [ items, setItems ] = useState<Item[]>([]);
+  const [ states, setStates ] = useState<string[]>([]);
+  const [ cities, setCities ] = useState<string[]>([]);
+
+  const [ selectedState, setSelectedState ] = useState('0');
+  const [ selectedCity, setSelectedCity ] = useState('0');
 
   useEffect(() => {
     api.get('items').then(response => {
       setItems(response.data);
     });
    }, []);
+
+  useEffect(() => {
+    axios.get<State[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+      const initials = response.data.map(uf => uf.sigla);
+      setStates(initials);
+    });
+  }, []);
+
+  useEffect(() => {
+    if ( selectedState === '0' )
+      return;
+
+    axios
+      .get<City[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedState}/municipios`)
+      .then(response => {
+        const names = response.data.map(city => city.nome);
+        setCities(names);
+    });
+  }, [selectedState]);
+
+  function handleSelectState(event: ChangeEvent<HTMLSelectElement> ) {
+    setSelectedState(event.target.value);
+  }
+
+  function handleSelectCity(event: ChangeEvent<HTMLSelectElement> ) {
+    setSelectedCity(event.target.value);
+  }
 
   return (
     <div id="create-point-page">
@@ -91,15 +132,29 @@ const CreatePoint = () => {
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">State/County</label>
-              <select name="uf" id="uf">
+              <select 
+                name="uf" 
+                id="uf"
+                onChange={handleSelectState}>
+
                 <option value="0">Select State/County</option>
+                {states.map(state => (
+                  <option key={state} value={state}>{state}</option>
+                ))}
               </select>
             </div>
 
             <div className="field">
               <label htmlFor="city">City</label>
-              <select name="city" id="city">
+              <select 
+                name="city" 
+                id="city"
+                onChange={handleSelectCity}>
+
                 <option value="0">Select City</option>
+                {cities.map(city => (
+                  <option key={city} value={city}>{city}</option>
+                ))}
               </select>
             </div>
           </div>
